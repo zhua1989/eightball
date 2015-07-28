1b264a0229dfaed7741985403d73a6c88fbe6826
Anthony Zhu <zhua89@gmail.com>
2015-07-28 14:10:02 -0400

    add code

--- ball.js
+++ ball.js
@@ -1,5 +1,36 @@
 var net = require('net');
-var fs = require('fs');
 var port = 2500;
 
 
+var eightBallArray = ["It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+]
+
+var server = net.createServer(function(connection) {
+    console.log("I'm connected")
+    connection.on('data', function(dataFromClient) {
+        connection.setEncoding("utf8")
+        var randomAnswer = eightBallArray[Math.floor(Math.random()*eightBallArray.length)];
+        connection.write(randomAnswer);
+
+    })
+})
+
+
+
+
+server.listen(port, function() {
+    console.log("Running and Listening " + port)
+})
